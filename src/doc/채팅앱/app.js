d4b539b9234e6aad75415248ca9ec7f8aaf638aa
Kimkunwoo <kkwkkw100@gmail.com>
2023-09-03 13:39:37 +0900

    feat 채팅페이지

---
+++ src/doc/채팅앱/app.js
@@ -0,0 +1,27 @@
+const express= require("express") //require 를쓰면 자동으로 node_modules를보기떄문에 경로지정이 불필요
+const http= require("http");//node에서 http모듈을 불러옴
+const { connect } = require("http2");
+const app =express();
+const path=require("path")//url을 만들기 쉽게? 도와주는 도구
+const server =http.createServer(app);//express로 만든 app을 서버로담아 express가 http로 실행되도록만듬
+const moment=require("moment")
+
+const socketIO= require("socket.io")//라이브러리를 불러옴
+const io= socketIO(server); //변수에 서버를 담음 이변수로 메시지를 주고 받음
+app.use(express.static(path.join(__dirname,"src")))//보여줄 폴더 지정 ,path join을 쓰는이유는 운영체제 마다 경로지정이 다르기 때문에
+
+const PORT = process.env.PORT || 5000;
+
+
+server.listen(PORT, ()=>console.log(`server is running${PORT}`))
+
+io.on("connection",(socket)=>{
+    socket.on("abc", (data)=>{
+        const{name, msg}= data;
+    io.emit("abc", {
+        name,
+        msg,
+        time:moment(new Date()).format("h:mm A")
+    })//오브젝트로 받은걸 보내준다?
+    })
+})//connection이 일어나면 모든 정보를 socket에 담는다
