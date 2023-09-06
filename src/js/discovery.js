493a3c5df01981f7027f3ae8e5ad6a9d8368624f
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-06 09:33:33 +0900

    feat: 디스커버리 - mock 데이터 추가

---
+++ src/js/discovery.js
@@ -0,0 +1,23 @@
+const posts = [
+  {
+    title: 'React [input] 개발자 모집 중!',
+    description: 'React.js + Typescript 가능하신 프런트 개발자 분 모집 중입니다! 매 주마다 정기회의 진행하며 코드 리뷰를 수행하고 있습니다.',
+    url: '/frame_15.png',
+  },
+  {
+    title: 'Node.js [input] 공부 스터디',
+    description: 'Node.js 에 대해서 학습하며 어떻게 동작하는 지 오픈소스 리포지토리를 기준으로 각자 파트를 맡고 분석하며 매주 1회 비대면회의로 발표합니다.',
+    url: '/nodejs_round.png',
+  },
+  {
+    title: '[input] 인생에 대한 회고 모임',
+    description: '인생이란 무엇일까',
+    url: '/usagi_01.webp',
+  },
+  {
+    title: 'Nest.js 서버 [input] 프로그래밍 스터디',
+    description: 'Nest.js 를 학습하며 최종적으로 MSA 프로젝트를 진행해 볼 스터디원 구하고 있습니다.',
+    url: '/nestjs.jpeg',
+  },
+];
+
